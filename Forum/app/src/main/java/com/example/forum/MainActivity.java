e581459c62dc739ebee27c093a7b5fe6a24e2f02
LiXiangji <u6729279@anu.edu.au>
2023-09-22 16:34:16 +1000

    connect

--- Forum/app/src/main/java/com/example/forum/MainActivity.java
+++ Forum/app/src/main/java/com/example/forum/MainActivity.java
@@ -6,16 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void jump(View v){
-        Intent intent=new Intent(this, LogIn.class);
-        startActivity(intent);
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://ga-23s2-a5f8f-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, helloleoo!");
     }
+
+    public void buttonFun(){
+
+    }
+
+
+
 }
