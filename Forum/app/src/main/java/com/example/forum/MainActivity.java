5d289d30c2457591af81966ccb7ada728a8331bf
LiXiangji <u6729279@anu.edu.au>
2023-09-24 18:48:17 +1000

    Change two login page

--- Forum/app/src/main/java/com/example/forum/MainActivity.java
+++ Forum/app/src/main/java/com/example/forum/MainActivity.java
@@ -10,6 +10,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean userLoggedIn = false; // 用户登录状态
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
     }
 
     public void jump(View v){
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://ga-23s2-a5f8f-default-rtdb.firebaseio.com/");
-        DatabaseReference myRef = database.getReference("message");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance("https://ga-23s2-a5f8f-default-rtdb.firebaseio.com/");
+//        DatabaseReference myRef = database.getReference("message");
+//        myRef.setValue("Hello, helloleoo!");
 
-        myRef.setValue("Hello, helloleoo!");
+        setContentView(R.layout.activity_log_in);
+        Intent intent = getIntent();
+        userLoggedIn = intent.getBooleanExtra("USER_LOGGED_IN", false);
+
+        if (!userLoggedIn) {
+            // 如果用户未登录，重定向到登录界面
+            Intent loginIntent = new Intent(this, LogIn.class);
+            startActivity(loginIntent);
+            finish();
+            return;
+        }
+
+        setContentView(R.layout.activity_main);//还未添加跳转页面
+
     }
 
     public void buttonFun(){
--- Forum/app/src/main/java/com/example/forum/write.java
+++
@@ -1,5 +0,0 @@
-package com.example.forum;
-
-public class write {
-
-}
