dd85def2e224341bd436658cd9acbde35219d15b
Wangtao Jia <u7370733@anu.edu.au>
2023-09-27 14:07:57 +1000

    Push for modifying applySearch()

--- Forum/app/src/main/java/com/example/forum/Main_Page.java
+++ Forum/app/src/main/java/com/example/forum/Main_Page.java
@@ -1,20 +1,27 @@
 package com.example.forum;
 
 import android.os.Bundle;
-import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ListView;
+import android.widget.MultiAutoCompleteTextView;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.forum.databinding.ActivityMainPageBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class Main_Page extends AppCompatActivity {
 
@@ -47,10 +54,65 @@ public class Main_Page extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_page);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        // apply search function
+        applySearch();
     }
 
-    public void applySearch(){
+    public void applySearch() {
 
+        // achieve search list view function
+
+        MultiAutoCompleteTextView multiAutoCompleteTextView;
+        ListView listView;
+        ArrayList<String> dataList; // Initialize data source TODO: Input suitable data source and adapter
+        ArrayAdapter<String> adapter; // Initialize adapter
+
+        multiAutoCompleteTextView = findViewById(R.id.input_search);
+        listView = findViewById(R.id.listview_houses);
+
+        dataList = new ArrayList<>(); // TODO:Add relative data
+        dataList.add("Bruce");
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(adapter);
+
+        // TODO:Set adapter and your own tokenizer
+        multiAutoCompleteTextView.setAdapter(adapter);
+        // set tokenizer, this can be changed later
+        multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.Tokenizer() {
+            @Override
+            public int findTokenStart(CharSequence text, int cursor) {
+                return 0;
+            }
+
+            @Override
+            public int findTokenEnd(CharSequence text, int cursor) {
+                return text.length();
+            }
+
+            @Override
+            public CharSequence terminateToken(CharSequence text) {
+                return text; // return text itself and don't add any separator
+            }
+        });
+
+        // Change and fill the textview
+        multiAutoCompleteTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.getFilter().filter(s, new Filter.FilterListener() {
+                    @Override
+                    public void onFilterComplete(int count) {
+                        listView.setVisibility(count > 0 ? View.VISIBLE : View.GONE);
+                    }
+                });
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
 
     @Override
