9b2a576ba1e06e0e58d18b6baf03b19b4a4fa80d
Linsheng <u7630421@anu.edu.au>
2023-10-10 21:55:55 +1100

    Add a singleton AVLTreeFactory to transform List to AVLTree.

--- Forum/app/src/main/java/com/example/forum/AVLTreeFactory.java
+++ Forum/app/src/main/java/com/example/forum/AVLTreeFactory.java
@@ -40,5 +40,18 @@ public class AVLTreeFactory {
         }
         return at;
     }
+    public HouseTree houseTreeCreator(List<String> data){
+        this.dataString=data;
 
+        String[] pairs1=dataString.get(0).split(";");
+
+        HouseTree ht=new HouseTree(new House(pairs1[0],pairs1[1],pairs1[2],Integer.parseInt(pairs1[3]),Integer.parseInt(pairs1[4]),pairs1[5],pairs1[6]));
+
+        for(int i=1;i<=dataString.size()-1;i++){
+            String[] pairs=dataString.get(i).split(";");
+            ht.insert(new House(pairs[0],pairs[1],pairs[2],Integer.parseInt(pairs[3]),Integer.parseInt(pairs[4]),pairs[5],pairs[6]));
+
+        }
+        return ht;
+    }
 }
