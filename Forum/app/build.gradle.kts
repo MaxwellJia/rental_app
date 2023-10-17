5184ff9ec7b1cd304f7cb91dd0a69d102dbe87d2
Xiaochen <u7630926@anu.edu.au>
2023-10-17 15:31:44 +1100

    update unitTest for token and HouseTree

--- Forum/app/build.gradle.kts
+++ Forum/app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("junit:junit:4.12")
+    implementation("junit:junit:4.12")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Forum/app/src/main/java/com/example/forum/TokenParseTest.java
+++ Forum/app/src/main/java/com/example/forum/TokenParseTest.java
@@ -1,54 +1,98 @@
 package com.example.forum;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class TokenParseTest {
 
+    private TokenParse parser;
+
+    @Before
+    public void setUp() {
+        parser = null;
+    }
+
     @Test
-    public void testExtractLocation() {
-        TokenParse parser = new TokenParse("Citdsd 500-600 4");
+    public void testLocationParsing() {
+        parser = new TokenParse("City");
         assertEquals("City", parser.getLocation());
 
-        parser = new TokenParse("Kindadw 3");
+        parser = new TokenParse("Kin3");
         assertEquals("Kingston", parser.getLocation());
 
-        // Test for an unknown location
-        parser = new TokenParse("Cdsds 700-750 1");
+        parser = new TokenParse("123 bar 456");
+        assertEquals("Barton", parser.getLocation());
+
+        // Edge case: Empty input
+        parser = new TokenParse("");
         assertNull(parser.getLocation());
     }
 
     @Test
-    public void testExtractMinMaxPrice() {
-        TokenParse parser = new TokenParse("Acton 2 600");
-        assertEquals(500, (int) parser.getpriceRange().get(0));
-        assertEquals(700, (int) parser.getpriceRange().get(1));
+    public void testMinMaxPriceParsing() {
+        parser = new TokenParse("1000-2000");
+        assertEquals(Arrays.asList(1000, 2000), parser.getpriceRange());
 
-        // Test for different price format
-        parser = new TokenParse("Acton 450-550");
-        assertEquals(450, (int) parser.getpriceRange().get(0));
-        assertEquals(550, (int) parser.getpriceRange().get(1));
+        parser = new TokenParse("2000-1000");
+        assertEquals(Arrays.asList(1000, 2000), parser.getpriceRange());
 
-        // Test for an invalid input
-        parser = new TokenParse("Acton 1");
+        parser = new TokenParse("2500");
+        assertEquals(Arrays.asList(2400, 2600), parser.getpriceRange());
+
+        // Edge case: No price info
+        parser = new TokenParse("City");
+        assertNull(parser.getpriceRange());
+
+        // Edge case: Invalid range format
+        parser = new TokenParse("1000-2000-3000");
+        assertEquals(parser.getpriceRange(),new ArrayList<>());
+        //assertNull(parser.getpriceRange());
+
+        // Edge case: Non-numeric values
+        parser = new TokenParse("abcd-efgh");
         assertNull(parser.getpriceRange());
     }
 
     @Test
-    public void testExtractBedrooms() {
-        TokenParse parser = new TokenParse("Turner 600 6");
-        assertEquals(6, parser.getBedrooms());
+    public void testBedroomParsing() {
+        parser = new TokenParse("3 bedrooms");
+        assertEquals(3, parser.getBedrooms());
 
-        parser = new TokenParse("Turner 600 4");
-        assertEquals(4, parser.getBedrooms());
-
-        // Test for no bedroom information
-        parser = new TokenParse("Turner 600");
+        // Edge case: More than 6 rooms
+        parser = new TokenParse("9 bedrooms");
         assertEquals(0, parser.getBedrooms());
 
-        // Test for an invalid input
-        parser = new TokenParse("Turner 600 7");
+        // Edge case: No bedroom info
+        parser = new TokenParse("City");
+        assertEquals(0, parser.getBedrooms());
+
+        // Edge case: Invalid format
+        parser = new TokenParse("three bedrooms");
         assertEquals(0, parser.getBedrooms());
     }
+
+    @Test
+    public void testComplexString() {
+        parser = new TokenParse("I want 3 bedrooms in kin with price range 1000-2000");
+        assertEquals("Kingston", parser.getLocation());
+        assertEquals(3, parser.getBedrooms());
+        assertEquals(Arrays.asList(1000, 2000), parser.getpriceRange());
+    }
+
+    @Test
+    public void testAnotherComplexString() {
+        parser = new TokenParse("4 br in nor for 3000");
+        assertEquals("North Adelaide", parser.getLocation());
+        assertEquals(4, parser.getBedrooms());
+        assertEquals(Arrays.asList(2900, 3100), parser.getpriceRange());
+    }
+
+    // Add more test cases as needed
 }
+
