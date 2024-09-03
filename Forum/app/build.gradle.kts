6445ceae2773d9b0acaf8e27c3f32bc273f4f4cc
Linsheng <u7630421@anu.edu.au>
2024-09-04 09:19:18 +1000

    report.md

--- Forum/app/build.gradle.kts
+++ Forum/app/build.gradle.kts
@@ -137,4 +137,14 @@ dependencies {
     // 解决异步问题
     implementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
 
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation ("com.facebook.android:facebook-android-sdk:latest.release")
+
 }
--- Forum/build.gradle.kts
+++ Forum/build.gradle.kts
@@ -4,6 +4,9 @@ buildscript {
 
 
     }
+    repositories {
+        mavenCentral()
+    }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
